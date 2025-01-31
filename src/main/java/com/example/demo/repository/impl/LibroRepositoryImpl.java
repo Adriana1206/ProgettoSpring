b6807517c77f382fdbe67406f1343b390081f020
Adriana <piccioneadriana@gmail.com>
2025-01-31 09:26:27 +0100

    feature CriteriaBuilder

--- src/main/java/com/example/demo/repository/impl/LibroRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/impl/LibroRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.example.demo.repository.impl;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,12 @@ import com.example.demo.model.Libro;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+
+import jakarta.persistence.criteria.Predicate;
 
 
 @Repository
@@ -21,20 +28,27 @@ public class LibroRepositoryImpl{
 		
 		
 		String searchPattern = "%" + search + "%";
-		
-		
-		//query JPA
-		String queryStr = "SELECT l FROM Libro l WHERE l.titolo LIKE :search";
-		
-		
-		// Esegui la query utilizzando il parametro di ricerca
-		//la query restituisce oggetti di tipo libro
-        TypedQuery<Libro> query = entityManager.createQuery(queryStr, Libro.class); //creo una query utilizzando la stringa queryStr
-        //imposta il valore del parametro :search
-        query.setParameter("search", searchPattern);
 
-        // Restituisci i risultati
-        return query.getResultList();
+	    // Ottieni l'entity manager
+	    CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+	    
+	    // Crea una query di tipo Libro
+	    CriteriaQuery<Libro> query = cb.createQuery(Libro.class);
+	    
+	    // Definisci la root per la query (la tabella principale)
+	    Root<Libro> libroRoot = query.from(Libro.class);
+	    
+	    // Crea la condizione per la ricerca sul titolo
+	    Predicate titoloPredicate = cb.like(libroRoot.get("titolo"),searchPattern);
+	    
+	    // Applica la condizione finale alla query
+	    query.where(titoloPredicate);
+	    
+	    // Esegui la query
+	    TypedQuery<Libro> typedQuery = entityManager.createQuery(query);
+	    
+	    // Restituisci i risultati
+	    return typedQuery.getResultList();
 		
 	}
 	
